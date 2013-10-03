4463136880f54406849d84ec0c8b4227633720be
winebarrel <sgwr_dts@yahoo.co.jp>
2013-10-03 23:01:57 +0900

    comment out reconnect process

--- lib/unicorn/rails_reconnect.rb
+++ lib/unicorn/rails_reconnect.rb
@@ -1,9 +1,11 @@
 module Unicorn::RailsReconnect
   def reconnect
+=begin
     reconnect_txt = Rails.root.join('tmp', 'reconnect.txt')
 
     if reconnect_txt.exist?
       ActiveRecord::Base.clear_all_connections!
     end
+=end
   end
 end
